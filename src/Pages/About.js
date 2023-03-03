58acf2315b08b9e2323e548e754140e10630760b
SanjaySokal <sokalsanjay@gmail.com>
2023-03-03 15:47:51 +0530

    changes about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,7 @@ import img2 from '../images/about-banner.png';
 import PageBanner from './PageBanner';
 import AboutImage from './Parts/AboutImage';
 import AboutTop from './Parts/AboutTop';
-import AboutGang from './Parts/AboutGang';
+// import AboutGang from './Parts/AboutGang';
 import AboutService from './Parts/AboutService';
 import AboutDigi from './Parts/AboutDigi';
 import HomeFaqIcons from "./Parts/HomeFaqIcons";
@@ -15,7 +15,7 @@ const About = () => {
             <PageBanner logo={null} top={["Economical", "Efficient", "Effective"]} h2={"About our company"} h4={"One of the top leading digital marketing agency"} p={"Tortor amet mauris eros enim. Curabitur tincidunt et sollicitudin lorem eu volutpat. In in venenatis elementum est rhoncus suspendisse elementum elit egestas."} img={img2} />
             <AboutImage />
             <AboutTop />
-            <AboutGang />
+            {/* <AboutGang /> */}
             <AboutService />
             <AboutDigi />
             <AboutVision />
